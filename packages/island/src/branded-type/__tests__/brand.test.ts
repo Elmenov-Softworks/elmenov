bcc57dab1a4215facf7705439f85a6ff4e8135b3
Alex Elmenov <alex.elmenov@gmail.com>
2024-11-19 18:33:05 +0300

    Fix test

--- packages/island/src/branded-type/__tests__/brand.test.ts
+++ packages/island/src/branded-type/__tests__/brand.test.ts
@@ -1,5 +1,4 @@
 import { AssertionException } from '../../exception/assertion.exception';
-import { RuntimeException } from '../../exception/runtime.exception';
 import { BrandStructure } from '../brand.struct';
 import { BrandType, BrandValidator } from '../brand.type';
 
@@ -9,26 +8,26 @@ describe('Branded Types', () => {
 
   type NonNegative = BrandType<number, 'NonNegative'>;
   const NonNegative = new BrandStructure<NonNegative>(validator);
-  
+
   it('should create a branded value', () => {
     const zero = NonNegative.identity(0);
 
     expect(zero).toBe(0);
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(0);
-  })
+  });
 
   it('should throw an RuntimeException when trying to create a negative branded value', () => {
     const testThrows = () => NonNegative.identity(-1);
 
-    expect(testThrows).toThrow(RuntimeException);
+    expect(testThrows).toThrow(AssertionException);
     expect(testThrows).toThrow(error);
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(-1);
-  })
+  });
 
   it('should validate the passed value as branded', () => {
-    expect(NonNegative.is(0)).toBeTruthy(); 
+    expect(NonNegative.is(0)).toBeTruthy();
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(0);
   });
@@ -36,13 +35,13 @@ describe('Branded Types', () => {
   it('should validate the passed branded value', () => {
     const zero = NonNegative.identity(0);
 
-    expect(NonNegative.is(zero)).toBeTruthy(); 
+    expect(NonNegative.is(zero)).toBeTruthy();
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(zero);
   });
 
   it('should not validate the passed value', () => {
-    expect(NonNegative.is(-1)).toBeFalsy(); 
+    expect(NonNegative.is(-1)).toBeFalsy();
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(-1);
   });
@@ -58,7 +57,7 @@ describe('Branded Types', () => {
     const zero = NonNegative.identity(0);
 
     (() => NonNegative.assert(zero))();
-    
+
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(zero);
   });
@@ -66,7 +65,7 @@ describe('Branded Types', () => {
   it('should throw an RuntimeException if the specified value does not pass validation', () => {
     const testThrows = () => NonNegative.assert(-1);
 
-    expect(testThrows).toThrow(RuntimeException);
+    expect(testThrows).toThrow(AssertionException);
     expect(testThrows).toThrow(error);
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(-1);
@@ -85,4 +84,4 @@ describe('Branded Types', () => {
     expect(validator).toHaveBeenCalled();
     expect(validator).toHaveBeenCalledWith(1);
   });
-})
+});
