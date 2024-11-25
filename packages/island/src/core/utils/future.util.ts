3da18fa94a5fd9091d7876e332ea272bb0e91272
Alex Elmenov <alex.elmenov@gmail.com>
2024-11-25 05:44:18 +0300

    Add future blank

---
+++ packages/island/src/core/utils/future.util.ts
@@ -0,0 +1,9 @@
+// type FutureTask<T> = (resolve: (value: T) => void, reject: (error: Error) => void) => void;
+
+// export class Future<T> {
+//   #task: FutureTask<T>;
+
+//   constructor(task: FutureTask<T>) {
+//     this.#task = task;
+//   }
+// }
